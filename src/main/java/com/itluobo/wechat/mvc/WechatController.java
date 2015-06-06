8a7cecd98d62c670f9a363e364c87047776cbaf3
kenvi <mrwhite@163.com>
2015-06-06 23:54:37 +0800

    add velocity

--- src/main/java/com/itluobo/wechat/mvc/WechatController.java
+++ src/main/java/com/itluobo/wechat/mvc/WechatController.java
@@ -5,13 +5,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HelloController {
+public class WechatController {
 
-	@RequestMapping("/greeting")
-	public String printWelcome(@RequestParam("echostr")String echostr, ModelMap model) {
+	@RequestMapping("/service/message")
+	public ModelAndView printWelcome(@RequestParam("echostr")String echostr, ModelMap model) {
 		model.addAttribute("message", echostr);
-		return "hello";
+
+		return new ModelAndView("validate",model);
 	}
 }
