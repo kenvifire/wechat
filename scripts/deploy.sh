386ce23646e00c0334cc876213ce29a136fd8ce6
kenvi <mrwhite@163.com>
2019-01-02 20:37:21 -0800

    add deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+DEPLOY_USER=$1
+ENV=$2
+PROJECT=$3
+
+cd /usr/${DEPLOY_USER}/${PROJECT}/${ENV}
+nohup java -jar itluobo-wechat-0.1.0.jar > service.log &
+echo "start service success"
+echo "travis build finished"
