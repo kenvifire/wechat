4ca29d0244e4f5321dedc968443864827eb7ac36
kenvi <mrwhite@163.com>
2019-01-03 23:25:56 -0800

    fix deployment bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,19 @@ cd /usr/${DEPLOY_USER}/${PROJECT}/${ENV}
 cp /usr/${DEPLOY_USER}/${PROJECT}/package/*.jar /usr/${DEPLOY_USER}/${PROJECT}/${ENV}/
 
 ##
-pid=$(lsof -i:${PORT} | grep java | awk '{print $2}')
+pid=$(/usr/sbin/lsof -i:${PORT} | grep java | awk '{print $2}')
 
-if [ -n $pid ]
-then kill -9 $pid
+if [ -z "$pid" ]
+then
+    echo "no process found"
+else
+    kill -9 $pid
 fi
 
 ##
-nohup java -jar *.jar --server.port=${PORT} > service.log &
+echo $(pwd)
+echo "start deploying service"
+nohup java  -jar *.jar --server.port=${PORT} > service.log &
+sleep 5
 echo "start service success"
 echo "travis build finished"
