185a35210761c8e9a703ec25b07ce3af0b3029d2
kenvi <mrwhite@163.com>
2019-01-03 21:56:21 -0800

    update deployment script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,12 @@ PORT=$4
 cd /usr/${DEPLOY_USER}/${PROJECT}/${ENV}
 cp /usr/${DEPLOY_USER}/${PROJECT}/package/*.jar /usr/${DEPLOY_USER}/${PROJECT}/${ENV}/
 
+##
+pid=$(lsof -i:${PORT} | grep java | awk '{print $2}')
+
+if [ -n $pid ]
+then kill -9 $pid
+fi
 
 ##
 nohup java -jar *.jar --server.port=${PORT} > service.log &
